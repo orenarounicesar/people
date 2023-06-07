6f60f2052c62862a4cdd7b119fca669294f67b3e
start.vaadin.com <no-reply@vaadin.com>
2023-06-07 02:35:47 +0000

    Generated project

---
+++ src/main/java/edu/orenarounicesar/people/data/service/SamplePersonRepository.java
@@ -0,0 +1,12 @@
+package edu.orenarounicesar.people.data.service;
+
+import edu.orenarounicesar.people.data.entity.SamplePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SamplePersonRepository
+        extends
+            JpaRepository<SamplePerson, Long>,
+            JpaSpecificationExecutor<SamplePerson> {
+
+}
